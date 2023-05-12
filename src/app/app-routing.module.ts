08e133c341683386a410291705d3617813b59595
nicosartori05 <nicolas.sartori@davinci.edu.ar>
2023-05-12 10:53:08 -0300

    creacion branch desarrollo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './vistas/login/login.component';
+import { RegistroComponent } from './vistas/registro/registro.component';
+import { ProductosComponent } from './vistas/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'registro',
+    component: RegistroComponent
+  },
+  {
+    path:'productos',
+    component: ProductosComponent
+  },
+  { 
+    path: '', 
+    redirectTo: '/', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: '**',
+   component: ProductosComponent 
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductosComponent } from './vistas/productos/productos.component';
+import { LoginComponent } from './vistas/login/login.component';
+import { RegistroComponent } from './vistas/registro/registro.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatMenuModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
